42323d20b8aeca46990068e56e7746d07dbda83f
shwetams <shweta.gupta.2012@live.com>
2016-12-30 10:23:37 +0530

    Add files via upload

--- Lesson3/ReceiveDeviceMessages/index.js
+++ Lesson3/ReceiveDeviceMessages/index.js
@@ -1,18 +1,31 @@
 /*
+
 * IoT Hub Raspberry Pi NodeJS - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+
 */
+
 'use strict';
 
+
+
 // This function is triggered each time a message is revieved in the IoTHub.
+
 // The message payload is persisted in an Azure Storage Table
+// npm install node-uuid
 var moment = require('moment');
 
+
+
 module.exports = function (context, iotHubMessage) {
+  var uniqueid = moment.utc().format('hhmmss') + process.hrtime()[1] + '';
   context.log('Message received: ' + JSON.stringify(iotHubMessage));
-  context.bindings.outputTable = {
-    "partitionKey": moment.utc().format('YYYYMMDD'),
-    "rowKey": moment.utc().format('hhmmss') + process.hrtime()[1] + '',
-    "message": JSON.stringify(iotHubMessage)
-  };
+
+    context.bindings.outputdocument = JSON.stringify({ 
+    id: uniqueid  + '',
+    message: JSON.stringify(iotHubMessage)
+  });
+
   context.done();
+
 };
+
